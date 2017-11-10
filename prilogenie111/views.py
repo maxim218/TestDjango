417bfda2cfac82207fe92ab3888ad8e3cd433e27
maxim218 <maxim1996>
2017-11-10 17:23:49 +0300

    Add templates

--- prilogenie111/views.py
+++ prilogenie111/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import MyFirstModel
 
 def my_page_first(request):
-    return render(request, 'prilogenie111/my_page_first.html', {})
+    my_records_arr = MyFirstModel.objects.order_by('published_date')
+    return render(request, 'prilogenie111/my_page_first.html', {'my_records_arr': my_records_arr})
 
 def my_page_second(request):
     return render(request, 'prilogenie111/my_page_second.html', {})
